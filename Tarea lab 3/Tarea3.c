e8f498704c3d9658530b4c50aa3b9f7ab53070c6
Gustavo Vergara <gustavo.vergara.p@usach.cl>
2022-05-31 07:19:47 -0400

    "Avance" Tarea 3

--- Tarea lab 3/Tarea3.c
+++ Tarea lab 3/Tarea3.c
@@ -34,9 +34,9 @@ int c; //Cantidad de cuarteles
 			TDAgrafo *G=crearGrafoVacio(i);	
             //caso 2: "Las siguientes c líneas"
             int* cLineas = (int*)malloc(c * sizeof(int));
-            for (size_t x = 0; x < i; x++){
+            for (size_t x = 0; x < c; x++){
                 fscanf(pf,"%d",&cLineas[x]);
-            }printf("cuarteles\n");
+            }
             //caso3: 
 			//dependiendo de las lineas de archivo, 
 			//1 para grafo no dirigido no ponderado
@@ -44,8 +44,8 @@ int c; //Cantidad de cuarteles
 				fscanf(pf,"%d %d %d",&a,&j,&w);
 				G->adyacencias[a-1][j-1] = w;
 				G->adyacencias[j-1][a-1] = w;
-			}printf("intesecciones\n");
-			fclose(pf);printf("cerrado\n");
+			}
+			fclose(pf);
 			return  G;	
 		}
 	}
@@ -66,8 +66,9 @@ int c; //Cantidad de cuarteles
 			TDAgrafo *G=crearGrafoVacio(i);	
             //caso 2: "Las siguientes c líneas"
             int* cLineas = (int*)malloc(c * sizeof(int));
-            for (size_t x = 0; x < i; x++){
+            for (size_t x = 0; x < c; x++){
                 fscanf(pf,"%d",&cLineas[x]);
+				imprimirArreglo(cLineas,c);
             }
             fclose(pf);
 			return  cLineas;	
@@ -76,14 +77,15 @@ int c; //Cantidad de cuarteles
 //BLOQUE PRINCIPAL
 int main(int argc, char const *argv[]){
     //Declaramos los argumentos como archivo de entrada y salida
-    archivoEntrada = argv[1]; printf("i");
+    archivoEntrada = argv[1];
     if (argc < 3){printf("\nDebe especificar un archivo de Entrada y otro de Salida.\n");}
-    archivoSalida = argv[2]; printf("2");
+    archivoSalida = argv[2];
     //Creamos un grafo representando el "pueblo" y un arreglo
-    TDAgrafo* grafo = leerPueblo(archivoEntrada); printf("a");
-    int* cuarteles = leerCuarteles(archivoEntrada); printf("b");
+    TDAgrafo* grafo = leerPueblo(archivoEntrada);
+    int* cuarteles = (int*)malloc(c * sizeof(int));
+	cuarteles = leerCuarteles(archivoEntrada);
     //Imprimimos el grafo y su arreglo
-    imprimirMatrizAdyacencia(grafo); printf("c");
-    imprimirArreglo(cuarteles,c); printf("d");
+    imprimirMatrizAdyacencia(grafo);
+    imprimirArreglo(cuarteles,c); printf("AYUDA\n");
 
 }
